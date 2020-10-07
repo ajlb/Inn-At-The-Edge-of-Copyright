dddffa6380b75bf48e7e9f7d293d6f8fb5593780
Plover Brown <rellwoos@gmail.com>
2020-10-06 18:09:58 -0700

    schema and seeds for preliminary database configuration

---
+++ db/SCHEMA.sql
@@ -0,0 +1,155 @@
+CREATE TABLE `heroku_dd2cb150d033ed5`.`players` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(320) NOT NULL,
+  `password` VARCHAR(200) NOT NULL,
+  `characterName` VARCHAR(20) NOT NULL,
+  `isLiving` TINYINT NOT NULL DEFAULT 1,
+  `isNPC` TINYINT NOT NULL DEFAULT 0,
+  `WIS` INT NOT NULL DEFAULT 0,
+  `DEX` INT NOT NULL DEFAULT 0,
+  `STR` INT NOT NULL DEFAULT 0,
+  `HP` INT NOT NULL DEFAULT 20,
+  `race` VARCHAR(45) NULL,
+  `class` VARCHAR(45) NULL,
+  `abilities` VARCHAR(45) NULL,
+  `inventory` TINYINT NOT NULL DEFAULT 0,
+  `backstory` TEXT NULL,
+  `description` VARCHAR(450) NULL,
+  `headSlot` INT NULL DEFAULT NULL,
+  `neckSlot` INT NULL DEFAULT NULL,
+  `torsoSlot` INT NULL DEFAULT NULL,
+  `rightHandSlot` INT NULL DEFAULT NULL,
+  `leftHandSlot` INT NULL DEFAULT NULL,
+  `legsSlot` INT NULL DEFAULT NULL,
+  `feetSlot` INT NULL DEFAULT NULL,
+  `ringSlot` INT NULL DEFAULT NULL,
+  `handsSlot` INT NULL DEFAULT NULL,
+  `twoHands` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+
+CREATE TABLE `heroku_dd2cb150d033ed5`.`locations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `locationName` VARCHAR(45) NOT NULL,
+  `locationDayDescription` VARCHAR(400) NOT NULL,
+  `locationNightDescription` VARCHAR(400) NOT NULL,
+  `exitN` VARCHAR(45) NULL,
+  `exitE` VARCHAR(45) NULL,
+  `exitS` VARCHAR(45) NULL,
+  `exitW` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+
+CREATE TABLE `heroku_dd2cb150d033ed5`.`weather` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `weatherCondition` VARCHAR(20) NOT NULL,
+  `dayDescription` VARCHAR(100) NULL,
+  `nightDescription` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+
+CREATE TABLE `heroku_dd2cb150d033ed5`.`actions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `actionName` VARCHAR(45) NOT NULL,
+  `function` VARCHAR(45) NULL,
+  `selfDescription` VARCHAR(200) NULL,
+  `roomDescription` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+
+CREATE TABLE `heroku_dd2cb150d033ed5`.`help` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `commandName` VARCHAR(45) NOT NULL,
+  `commandBriefDescription` VARCHAR(45) NOT NULL,
+  `commandLongDescription` VARCHAR(300) NOT NULL,
+  `waysToCall` VARCHAR(200) NOT NULL,
+  `exampleCall` VARCHAR(45) NOT NULL,
+  `exampleResult` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+
+CREATE TABLE `heroku_dd2cb150d033ed5`.`quests` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `questTitle` VARCHAR(45) NOT NULL,
+  `Line1` VARCHAR(400) NOT NULL,
+  `Line2` VARCHAR(400) NULL,
+  `Line3` VARCHAR(400) NULL,
+  `Line4` VARCHAR(400) NULL,
+  `Line5` VARCHAR(400) NULL,
+  `questToken` BIGINT NULL,
+  `Hint1` VARCHAR(400) NOT NULL,
+  `Hint2` VARCHAR(400) NULL,
+  `Hint3` VARCHAR(400) NULL,
+  `XPorItem` TINYINT NOT NULL,
+  `reward` BIGINT NOT NULL,
+  `completionItem` BIGINT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+
+CREATE TABLE `heroku_dd2cb150d033ed5`.`items` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `itemName` VARCHAR(45), NOT NULL,
+  `description` VARCHAR(200) NOT NULL,
+  `inventory` TINYINT NOT NULL DEFAULT 0,
+  `inventorySize` BIGINT NULL DEFAULT NULL,
+  `headSlot` TINYINT NOT NULL DEFAULT 0,
+  `neckSlot` TINYINT NOT NULL DEFAULT 0,
+  `torsoSlot` TINYINT NOT NULL DEFAULT 0,
+  `rightHandSlot` TINYINT NOT NULL DEFAULT 0,
+  `leftHandSlot` TINYINT NOT NULL DEFAULT 0,
+  `legsSlot` TINYINT NOT NULL DEFAULT 0,
+  `feetSlot` TINYINT NOT NULL DEFAULT 0,
+  `ringSlot` TINYINT NOT NULL DEFAULT 0,
+  `handsSlot` TINYINT NOT NULL DEFAULT 0,
+  `twoHands` TINYINT NOT NULL DEFAULT 0,
+  `edible` TINYINT NOT NULL DEFAULT 0,
+  `healthEffect` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+
+CREATE TABLE `heroku_dd2cb150d033ed5`.`inventory` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `character_id` BIGINT NOT NULL,
+  `item_id` BIGINT NOT NULL,
+  `quantity` BIGINT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+
+  CREATE TABLE `heroku_dd2cb150d033ed5`.`races` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `raceName` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(450) NOT NULL,
+  `STRbonus` INT NOT NULL DEFAULT 0,
+  `DEXbonus` INT NOT NULL DEFAULT 0,
+  `WISbonus` INT NOT NULL DEFAULT 0,
+  `HPbonus` INT NOT NULL,
+  `specialAbility` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+
+CREATE TABLE `heroku_dd2cb150d033ed5`.`classes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `className` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(450) NOT NULL,
+  `specialItem` INT NULL DEFAULT NULL,
+  `level1Ability` INT NOT NULL,
+  `level2Ability` INT NOT NULL,
+  `level3Ability` INT NOT NULL,
+  `level4Ability` INT NOT NULL,
+  `level5Ability` INT NOT NULL,
+  `level6Ability` INT NOT NULL,
+  `level7Ability` INT NOT NULL,
+  `level8Ability` INT NOT NULL,
+  `level9Ability` INT NOT NULL,
+  `level10Ability` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
---
+++ db/SEEDS.sql
@@ -0,0 +1,50 @@
+INSERT INTO heroku_dd2cb150d033ed5.weather (`weatherCondition`, `dayDescription`, `nightDescription`)
+VALUES
+("clear sky", "The sun shines bright in the sky above you, with nary a cloud in sight.", "The moon shines bright above you in the star-splashed night sky."),
+("few clouds", "The sun is shining high in the sky, with pretty white clouds wafting along in the breeze.", "The moon shines down on you from a starry sky with a few dark shadows of clouds."),
+("scattered clouds", "The sky is gray with clouds, spread evenly across the sky like a dour blanket.", "The night is dark, with clouds covering the moon and stars."),
+("broken clouds", "The sky is patchy with clouds and blue, shards of light shining through briefly only to be obscured by clouds.", "The night sky shows patches of shadow and patches of starry brightness."),
+("shower rain", "Rain spatters down from a cloudy sky, but bits of blue show here and there.", "Rain mists down from the dark night sky."),
+("rain", "Rain pours down from a sky uniformly covered by gray clouds.", "Rain pours down from the dark sky."),
+("thunderstorm", "Rain is sheeting down from ominously dark clouds lit by slashes of lightning. Thunder booms from time to time.", "Rain sheets down as the dark night is pierced by shards of lightning and the booming crack of thunder."),
+("snow", "Snow drifts down from the white sky.", "Snow drifts down in the darkness with a soft patter."),
+("mist", "The landscape is partially obscured by a damp mist.", "Wavering moonlight filters down through the damp mist.");
+
+
+INSERT INTO actions (actionName, function, selfDescription, roomDescription)
+VALUES ("get", "getItem()", "You pick up a ", " picks up a "),
+("look", "lookAround()", "You look around.", " glances around."),
+("drop", "dropItem()", "You drop a ", " drops a "),
+("inventory", "checkInventory()", Null, Null); 
+
+
+INSERT INTO help (commandName, commandBriefDescription, commandLongDescription, waysToCall, exampleCall, exampleResult)
+VALUES
+("get", "Pick up an item.", "When you type a get command word followed by an item in your location that is free to pick up, you will add it to your inventory. If the item isn't free to pick up, you will get a result of 'You can't pick that up!'", "get (g), pick up, grab, take", "get the mushroom", "You pick up a mushroom."),
+("drop", "Drop an item.", "When you type a drop command word followed by an item in your inventory, you will drop it into your location.", "drop (d), discard", "drop mushroom", "You drop a mushroom."),
+("look", "You look around you", "When you type a look command word with nothing after it, you will look around your location, seeing the description, possible exits, and items nearby.", "look (l), look around", "look around", "You look around. \n An Empty Room: \n Four bare, white walls surround you. A simple bed and a chest are the room's only furniture. A wooden door leads west.");
+
+
+INSERT INTO locations (locationName, locationDescription, exitN, exitE, exitS, exitW, locationInventory)
+VALUES ("Pumpkin Patch Center", "You are at the heart of a pumpkin patch. Though on the vine, and uncarved, the pumpkins you see out of the corners of your vision seem to be leering evilly at you, like jack-o-lanterns.", 11, 21, 31, 41, "PumpkinPatchCenterInventory"),
+("Pumpkin Patch North", "The pumpkin patch stretches the the east, west and south of you. The pumpkins surrounding you are craggy and crooked, and the longer you look at them, the creepier they seem.", Null, 51, 1, 61, "PumpkinPatchNorthInventory"),
+("Pumpkin Patch East", "The pumpkin patch stretches to the north, west, and south of you. The pumpkins around you a unnervingly creepy. Uncarverd pumpkins shouldn't be creepy, should they?", 51, Null, 71, 1, "PumpkinPatchEastInventory");
+
+
+INSERT INTO quests (questTitle, Line1, Line2, Hint1, XPorItem, reward, completionItem)
+VALUES ("Find my missing sock.", "I'm pretty sure I put it in the washing machine, but it didn't come out of the dryer. Can you help me?", "Oh thank you! I'm so glad. I live at 4532 Little Bittle Lane, out east of the inn.", "I suppose you could my closet, but I already looked twice...", True, 500, 101);
+
+
+INSERT INTO items (itemName, description, category)
+VALUES
+("branch", "a dry, brown branch", "nature"),
+("mushroom", "a small, white mushroom with a round top", "nature"),
+("sock", "a lone argyle sock, blue and yellow", "household"),
+("pumpkin pie", "a whole pumpkin pie! It smells like autumn", "food");
+
+
+INSERT INTO players (email, password, characterName, isLiving, isNPC, WIS, DEX, STR, HP, race, class, inventory, backstory, description, headSlot, neckSlot, torsoSlot, rightHandSlot, leftHandSlot, legsSlot, feetSlot, ringSlot, handsSlot, twoHands)
+VALUES
+("moop@mop.com", "verysecure", "Felix", True, True, 10, 10, 10, 50, "Cat", "Rogue", True, "Felix has lived in the inn since he showed up as a young cat. He came in one morning when the innkeeper opened the door, and never left.", "A black and white cat with short hair, and a slight notch in their right ear", 0, 121, 0, 0, 0, 0, 0, 0, 0, 0);
+
+
