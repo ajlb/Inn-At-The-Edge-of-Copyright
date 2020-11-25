5744516f2d11570e98b48ecff4879a03eedb8830
KIL Bismuth <kiralowrey@gmail.com>
2020-11-24 22:21:23 -0800

    improved profile.js

--- client/src/pages/Player/profile.js
+++ client/src/pages/Player/profile.js
@@ -2,34 +2,54 @@
 // THIS WILL NOT BE USED
 // IT'S JUST FOR REFERENCE
 
-import React from "react";
-import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+// import React from "react";
+// import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
-const Profile = () => {
-  const { user } = useAuth0();
-  const { name, picture, email } = user;
-  return (
-    <div>
-      <div className="row align-items-center profile-header">
-        <div className="col-md-2 mb-3">
-          <img
-            src={picture}
-            alt="Profile"
-            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-          />
-        </div>
-        <div className="col-md text-center text-md-left">
-          <h2>{name}</h2>
-          <p classname="lead text-muted">{email}</p>
-        </div>
-      </div>
-      <div className="row">
-        <pre className="col-12 text-light bg-dark p-4">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div>
-    </div>
-  );
-};
+// const Profile = () => {
+//   const { user } = useAuth0();
+//   const { name, picture, email } = user;
+//   return (
+//     <div>
+//       <div className="row align-items-center profile-header">
+//         <div className="col-md-2 mb-3">
+//           <img
+//             src={picture}
+//             alt="Profile"
+//             className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+//           />
+//         </div>
+//         <div className="col-md text-center text-md-left">
+//           <h2>{name}</h2>
+//           <p classname="lead text-muted">{email}</p>
+//         </div>
+//       </div>
+//       <div className="row">
+//         <pre className="col-12 text-light bg-dark p-4">
+//           {JSON.stringify(user, null, 2)}
+//         </pre>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Profile;
+// OR
+
+// const Profile = () => {
+//   const { user, isAuthenticated, isLoading } = useAuth0();
+
+//   if (isLoading) {
+//     return <div>Loading ...</div>;
+//   }
+
+//   return (
+//     isAuthenticated && (
+//       <div>
+//         <img src={user.picture} alt={user.name} />
+//         <h2>{user.name}</h2>
+//         <p>{user.email}</p>
+//       </div>
+//     )
+//   );
+// };
+
+// export default Profile;
