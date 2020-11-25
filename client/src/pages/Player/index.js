ea83de51cbd32b2cb711076a1d9da97218f3d17c
KIL Bismuth <kiralowrey@gmail.com>
2020-11-24 22:14:38 -0800

    added Player page placeholder

---
+++ client/src/pages/Player/index.js
@@ -0,0 +1,7 @@
+// This is a placeholder file for when we add a page for the player to look at
+
+// 1. their (non private) login information, so they know they're on the right account
+// 2. Their player name, race, and profession
+// 3. Their stats
+// 4. their held items and worn items
+// 5. anything else
