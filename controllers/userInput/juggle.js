76e706aba34b856e4bf8da736ce678932293edc2
Plover Brown <rellwoos@gmail.com>
2020-12-03 09:20:53 -0800

    fixed juggle to work with new inventory

--- controllers/userInput/juggle.js
+++ controllers/userInput/juggle.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 function incrementDex(user) {
     return new Promise(function (resolve, reject) {
-        db.Player.findOneAndUpdate({ characterName: user }, { $inc: { "stats.DEX": 0.1 } }, { new: true }).then(data => {
+        db.Player.findOneAndUpdate({ characterName: user }, { $inc: { "stats.DEX": 0.1 } }, { new: true }).populate('inventory.item').then(data => {
             resolve(data);
         });
     });
