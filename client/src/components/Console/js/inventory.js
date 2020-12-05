70577093cbb26b0f79a25a697c3d1871976ea7f5
Plover Brown <rellwoos@gmail.com>
2020-12-05 11:48:47 -0800

    a couple of minor fixes to inventory

--- client/src/components/Console/js/inventory.js
+++ client/src/components/Console/js/inventory.js
@@ -1,15 +1,21 @@
+import pluralize from "pluralize";
 
 function showInventory(user, setChatHistory) {
     console.log(user)
     const inventoryArray = [];
     const userInventory = user.inventory;
 
+    inventoryArray.push(`\xa0\xa0\xa0\xa0`);
+    inventoryArray.push(`You are carrying: `);
+
     userInventory.forEach(param => {
-        inventoryArray.push(`${param.quantity} ${param.item.itemName}`);
+        inventoryArray.push(`${param.quantity} ${pluralize(param.item.itemName, param.quantity)}`);
     })
+    inventoryArray.push(`\xa0\xa0\xa0\xa0`);
+
 
     if(inventoryArray.length === 0){
-        setChatHistory(prevState => [...prevState, {type: 'displayed-indent', text: `Your inventory is empty`}]);
+        setChatHistory(prevState => [...prevState, {type: 'displayed-indent', text: `Your inventory is empty!`}]);
         
     }
 
