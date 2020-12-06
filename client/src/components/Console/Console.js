b0bc950da35b5bebb096d0ff1f1fbe5bfefe3218
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-12-05 16:05:19 -0800

    test

--- client/src/components/Console/Console.js
+++ client/src/components/Console/Console.js
@@ -6,15 +6,15 @@ import { isBrowser } from 'react-device-detect';
 import GamewideInfo from '../../clientUtilities/GamewideInfo';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import socket from "../../clientUtilities/socket";
+import "./css/styles.css";
+import LoginButton from "../auth/LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "../auth/LogoutButton";
 console.log('==========================================')
 console.log('==========================================')
 console.log(process.env.REACT_APP_SOCKET_STRING)
 console.log('==========================================')
 console.log('==========================================')
-import "./css/styles.css";
-import LoginButton from "../auth/LoginButton";
-import { useAuth0 } from "@auth0/auth0-react";
-import LogoutButton from "../auth/LogoutButton";
 
 function Console() {
   //set state for whether to move to min state (because of soft keyboard on mobile)
