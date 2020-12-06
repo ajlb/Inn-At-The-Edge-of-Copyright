d1ce8e1fd3d07abfbab1d226075b889e934f1364
Nancy Lambert-Brown <2643160+n-lambert@users.noreply.github.com>
2020-12-05 16:00:33 -0800

    adding additional header

--- config/io-config.js
+++ config/io-config.js
@@ -3,7 +3,7 @@ module.exports = function (server) {
         cors: {
             origin: ["http://localhost:3000", "http://localhost:3001", `nicksnpcs.herokuapp.com:*`, `innv2-staging.herokuapp.com:*`, `innattheedge.herokuapp.com:*`, `innv2.herokuapp.com:*`],
             methods: ["GET", "POST"],
-            allowedHeaders: ["my-custom-header"],
+            allowedHeaders: ["my-custom-header", "heroku-session-affinity"],
             credentials: true
         }
     })
