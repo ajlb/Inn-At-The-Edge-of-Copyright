81865277b3bba8c3f49601bd4d9b3191e5e15f57
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-12-06 17:41:49 -0800

    attempt at https issue fix

--- server.js
+++ server.js
@@ -12,13 +12,11 @@ const backEngine = require("./controllers/backEngine");
 const APIBackroutes = require("./routes/API/backEngineAPI");
 const APIroutes = require("./routes/API/APIroutes");
 const APIadminRoutes = require("./routes/API/adminAPI");
-
-
+const enforce = require('express-sslify');
 // creating the io variable via the config folder
 const io = require('./config/io-config')(server);
 // setting all the server listeners
 require('./controllers/socketController')(io);
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,36 +25,28 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 /* API ROUTES */
-
 app.use("/backAPI", APIBackroutes);
 app.use("/frontAPI", APIroutes);
 app.use("/adminAPI", APIadminRoutes);
-
 //connect to mongoDB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/innattheedge", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 const connection = mongoose.connection;
-
-
-
 // log once mongoDB is open
 connection.once("open", function () {
   console.log("\nConnected to mongoose\n\n--------------begin log--------------\n");
 });
-
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
 server.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
