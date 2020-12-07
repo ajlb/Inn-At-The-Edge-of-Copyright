4be285c77dea2dec19ae58aed052a4b1cc4da37c
Plover Brown <rellwoos@gmail.com>
2020-12-07 13:57:56 -0800

    stats displaying incorrect items fix, and added colors

--- client/src/components/Console/js/stats.js
+++ client/src/components/Console/js/stats.js
@@ -1,21 +1,54 @@
+import { startSession } from "mongoose";
+
 //import setChatHistory from '../ChatPanel'
+const paramNames = {
+    DEX: 'Dexterity',
+    WIS: 'Wisdom',
+    STR: 'Strength',
+    CHA: 'Charisma',
+    HP: 'HP',
+}
 
 
 function showStats(user, setChatHistory) {
     console.log(user)
     const statsArray = [];
 
-    for (const param in user.stats) {
+    statsArray.push(`\xa0\xa0\xa0`);
+    statsArray.push(`You are a level ${user.stats.level} ${user.race} ${user.profession}.`);
+    statsArray.push(`You have ${user.stats.XP} experience.`);
+    statsArray.push(`\xa0\xa0\xa0`);
 
-        statsArray.push(`${param}:  ${Math.floor(user.stats[param])}`);
+    for (const param in user.stats) {
+        if (Object.keys(paramNames).includes(param)){
+            if (param === "HP"){
+                let color;
+                let HP = user.stats.HP;
+                let max = user.stats.maxHP;
+                if (HP === max){
+                    color = "text-success";
+                } else if (HP < 5){
+                    color = "text-danger";
+                } else {
+                    color = "text-warning";
+                }
+                statsArray.push(`${paramNames[param]}:  <span className='${color}'>${Math.floor(user.stats[param])}</span>/${Math.floor(user.stats.maxHP)}`);
+            } else {
+                if (user.stats[param] === user.stats["max"+param]){
+                    statsArray.push(`${paramNames[param]}:  <span className='displayed-cyan'>${Math.floor(user.stats[param])}</span> - You have reached the maximum level of ${paramNames[param]}`);
+                } else {
+                    statsArray.push(`${paramNames[param]}:  ${Math.floor(user.stats[param])}`);
+                }
+            }
+        }
     }
 
-    const deletedItems = statsArray.slice(0, 4);
-    const newDeletedItems = statsArray.slice(8, 10);
+    // const deletedItems = statsArray.slice(0, 4);
+    // const newDeletedItems = statsArray.slice(8, 10);
 
-    const newArray = deletedItems.concat(newDeletedItems)
+    // const newArray = deletedItems.concat(newDeletedItems)
 
-    newArray.forEach((user) => {
+    statsArray.forEach((user) => {
 
         setChatHistory(prevState => [...prevState, { type: 'displayed-indent', text: `${user}` }]);
         // console.log(newArray);
