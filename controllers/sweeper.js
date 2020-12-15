8535ddbf4fe949ae9e5d943c5a91c2d197b2050e
Plover Brown <rellwoos@gmail.com>
2020-12-14 17:53:34 -0800

    got sweeper registering both missing items and overflow items

--- controllers/sweeper.js
+++ controllers/sweeper.js
@@ -8,61 +8,73 @@ function checkLocale(index, allLocations) {
         const realTimeInventory = allLocations[index].inventory;
         const seedInventory = locationSeed[index].inventory;
         const diffObject = {};
+        // console.log(allLocations[index].locationName);
         for (const realItem of realTimeInventory) {
 
             matchedInSeedArray = seedInventory.filter(item => {
-                if (item.item['$oid'] === realItem.item.toString()){
+                if (item.item['$oid'] === realItem.item.toString()) {
                     return item;
                 }
             });
 
-            if (matchedInSeedArray.length === 1){
+            if (matchedInSeedArray.length === 1) {
                 let quantityToAdd = 0;
                 let quantityToTrash = 0;
-                if (matchedInSeedArray[0].quantity > realItem.quantity){
+                // console.log("SEED:", matchedInSeedArray[0].quantity);
+                // console.log("REAL:", realItem.quantity);
+                if (matchedInSeedArray[0].quantity > realItem.quantity) {
+                    // console.log('GOT A MINUS');
                     quantityToAdd += matchedInSeedArray[0].quantity - realItem.quantity;
-                } else if (realItem.quantity > matchedInSeedArray[0].quantity){
+                    // console.log("quantity to add:", quantityToAdd);
+                    if (quantityToAdd > 0) {
+                        diffObject[realItem.item] = quantityToAdd;
+                    }
+                } else if (realItem.quantity > matchedInSeedArray[0].quantity) {
+                    quantityToTrash = realItem.quantity - matchedInSeedArray[0].quantity;
                     let timesToRemove = realItem.dropTime.filter(time => {
-                        console.log(((new Date() - new Date(time)) * (1000 * 60)) > 2);
-                        if (((new Date() - new Date(time)) * (1000 * 60)) > 2){
+                        // console.log(((new Date() - new Date(time)) * (1000 * 60)) > 2);
+                        if (((new Date() - new Date(time)) * (1000 * 60)) > 2) {
                             return time
                         }
                     })
+                    if (timesToRemove.length < quantityToTrash) {
+                        quantityToTrash = timesToRemove.length;
+                    }
+                    // console.log("number to remove", quantityToTrash, realItem.item);
+                    if (quantityToTrash > 0) {
+                        diffObject[realItem.item] = quantityToTrash;
+                    }
                 }
-                // if (quantityToTrash > 0) {
-                //     diffObject[realTimeInventory[itemIndex].item] = quantityToTrash;
-                // }
-                // if (quantityToAdd > 0) {
-                //     diffObject[realTimeInventory[itemIndex].item] = quantityToAdd;
-                // }
             } else {
-                console.log("NO MATCH");
+                // console.log("UNWANTED ITEM:", realItem.quantity);
+                let timesToRemove = realItem.dropTime.filter(time => {
+                    // console.log(((new Date() - new Date(time)) * (1000 * 60)) > 2);
+                    if (((new Date() - new Date(time)) * (1000 * 60)) > 2) {
+                        return time
+                    }
+                })
+                // console.log("quantity to remove of unmatched item:", timesToRemove.length, realItem.item);
             }
 
-            // for (const seedItemIndex in seedInventory) {
-            //     //item is in room seed
-            //     // console.log(typeof seedInventory[seedItemIndex].item['$oid']);
-            //     // console.log(typeof realItem.item.toString());
-            //     // console.log(realItem.item.toString() == seedInventory[seedItemIndex].item['$oid']);
-
-            //     //item is not in room seed
-            //     console.log(realItem);
-            //     for (time of realItem.dropTime) {
-            //         console.log(((new Date() - new Date(time)) / (1000 * 60)) > 1);
-            //         if (((new Date() - new Date(time)) / (1000 * 60)) > 1) {
-            //             console.log("old item!");
-            //         }
-            //     }
-
-            // }
-            resolve(true)
         }
-
-        // if (Object.keys(diffObject).length === 0) {
-        //     resolve(false);
-        // } else {
-        //     resolve(diffObject);
-        // }
+        for (const seedItem of seedInventory) {
+            let matchingItems = realTimeInventory.filter(item => {
+                return item.item.toString() === seedItem.item["$oid"].toString();
+            })
+            // console.log(matchingItems.length);
+            if (matchingItems.length === 0) {
+                quantityToAdd = seedItem.quantity;
+                diffObject[seedItem.item["$oid"].toString()] = quantityToAdd;
+                // console.log("quantity of unmatched items to add:", quantityToAdd, seedItem.item["$oid"].toString());
+            }
+        }
+        if (Object.keys(diffObject).length === 0) {
+            resolve(false);
+        } else {
+            diffObject.locationName = allLocations[index].locationName
+            console.log(diffObject);
+            resolve(diffObject);
+        }
     });
 }
 
