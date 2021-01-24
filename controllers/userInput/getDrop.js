bd33999d72f3087515496aba22ac0fc23c38a494
Plover Brown <rellwoos@gmail.com>
2021-01-23 20:30:44 -0800

    added character name matching to give, copied from whisper

--- controllers/userInput/getDrop.js
+++ controllers/userInput/getDrop.js
@@ -192,7 +192,30 @@ function dropItem(socket, io, target, itemId, user, location) {
     })
 }
 
-function giveItem(socket, io, target, item, itemId, user, location) {
+function giveItem(socket, io, target, item, itemId, user, location, players) {
+        // How this works:
+    //   This for loop is going to see if the message received from the user starts with a player name
+    //   Because player names are only allowed to be three words max(two spaces),
+    //   this for loop iterates through the first three words of the users message starting at 3 and working its way down
+    // if at any point in the loop it recognizes a player's name, it will set the playerTo variable to that player's name
+    let playerExists = false;
+    for (let i = 2; i >= 0; i--) {
+        const potentialTarget = target.toLowerCase().split(' ').slice(0, i + 1).join(' ');
+        console.log("potential target:", potentialTarget);
+        players.forEach(player => {
+            if (player.toLowerCase() === potentialTarget || player.toLowerCase().startsWith(potentialTarget)) {
+                console.log(`${socket.nickname}'s whisper found ${potentialTarget}`);
+                target = player;
+                playerExists = true;
+            }
+        })
+    }
+    if (!playerExists) {
+        console.log('there was no matching player');
+        io.to(socket.id).emit('failure', "There is nobody here by that name.");
+        return false;
+    }
+
     //remove item from giver's inventory
     decrementItemUpdateOne(itemId, user, "player").then(returnData => {
         scrubInventoryReturnData(user, "player").then(returnData => {
